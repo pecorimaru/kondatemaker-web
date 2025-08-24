e7280d8d0e99f59d671045795a5d00abdda7c98c
pecorimaru <cornflakesplain@gmail.com>
2025-08-24 16:11:07 +0900

    Initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+// 基本的なテスト：数学ライブラリのテスト
+it('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
+
+// 基本的なテスト：文字列のテスト
+it('string concatenation works', () => {
+  expect('hello' + ' ' + 'world').toBe('hello world');
+});
